20b1f3991fd3610cba10dc782c661e0b3a3dd9fa
Stephen Finucane <stephen@that.guru>
2020-06-15 20:57:54 +0100

    Resolve style issues
    Use 'pre-commit run -a' to fix the code for us. This runs 'black' under
    the hood, which is an opinionated (read: non-configurable) style
    checker.
    
    Signed-off-by: Stephen Finucane <stephen@that.guru>

--- test_filter.py
+++ test_filter.py
@@ -8,72 +8,79 @@ from selenium.webdriver import Chrome
 
 @pytest.fixture
 def browser():
-  driver = Chrome()
-  driver.implicitly_wait(10)
-  yield driver
-  driver.quit()
-  
-   
+    driver = Chrome()
+    driver.implicitly_wait(10)
+    yield driver
+    driver.quit()
+
+
 # fails due to bug in application
 
+
 def test_filter_by_series(browser):
-  
-  patchwork_page = PatchworkHomePage(browser)
-  patchwork_page.load()
-  patchwork_page.show_filters() 
-  
-  patchwork_page.filter_by_series('fix series querry')
-  filtered_results = FilteredPatchResults(browser)
-  assert filtered_results.active_filters_contains('Series') > -1
-  
-# fails due to bug in application 
+
+    patchwork_page = PatchworkHomePage(browser)
+    patchwork_page.load()
+    patchwork_page.show_filters()
+
+    patchwork_page.filter_by_series("fix series querry")
+    filtered_results = FilteredPatchResults(browser)
+    assert filtered_results.active_filters_contains("Series") > -1
+
+
+# fails due to bug in application
+
 
 def test_filter_by_submitter(browser):
 
-  patchwork_page = PatchworkHomePage(browser)
-  patchwork_page.load()
-  patchwork_page.show_filters() 
-     
-  patchwork_page.filter_by_submitter('Stephen Finucane')
-  filtered_results = FilteredPatchResults(browser)
-  assert filtered_results.active_filters_contains('Submitter') > -1
-  
+    patchwork_page = PatchworkHomePage(browser)
+    patchwork_page.load()
+    patchwork_page.show_filters()
+
+    patchwork_page.filter_by_submitter("Stephen Finucane")
+    filtered_results = FilteredPatchResults(browser)
+    assert filtered_results.active_filters_contains("Submitter") > -1
+
+
 def test_filter_by_state(browser):
-  
-  patchwork_page = PatchworkHomePage(browser)
-  patchwork_page.load()
-  patchwork_page.show_filters() 
-  
-  patchwork_page.filter_by_state('New')
-  filtered_results = FilteredPatchResults(browser)
-  assert filtered_results.active_filters_contains('State') > 1
-  
+
+    patchwork_page = PatchworkHomePage(browser)
+    patchwork_page.load()
+    patchwork_page.show_filters()
+
+    patchwork_page.filter_by_state("New")
+    filtered_results = FilteredPatchResults(browser)
+    assert filtered_results.active_filters_contains("State") > 1
+
+
 def test_filter_by_search_term(browser):
-  
-  patchwork_page = PatchworkHomePage(browser)
-  patchwork_page.load()
-  patchwork_page.show_filters()
-  
-  patchwork_page.filter_by_search_term('query')
-  filtered_results = FilteredPatchResults(browser)
-  assert filtered_results.active_filters_contains('Search') > 1
-  
+
+    patchwork_page = PatchworkHomePage(browser)
+    patchwork_page.load()
+    patchwork_page.show_filters()
+
+    patchwork_page.filter_by_search_term("query")
+    filtered_results = FilteredPatchResults(browser)
+    assert filtered_results.active_filters_contains("Search") > 1
+
+
 def test_filter_by_archived(browser):
-  
-  patchwork_page = PatchworkHomePage(browser)
-  patchwork_page.load()
-  patchwork_page.show_filters() 
-  
-  patchwork_page.filter_by_archived('both')
-  filtered_results = FilteredPatchResults(browser)
-  assert filtered_results.active_filters_contains('Archived') > 1
-  
+
+    patchwork_page = PatchworkHomePage(browser)
+    patchwork_page.load()
+    patchwork_page.show_filters()
+
+    patchwork_page.filter_by_archived("both")
+    filtered_results = FilteredPatchResults(browser)
+    assert filtered_results.active_filters_contains("Archived") > 1
+
+
 def test_filter_by_delegate(browser):
-  
-  patchwork_page = PatchworkHomePage(browser)
-  patchwork_page.load()
-  patchwork_page.show_filters() 
-  
-  patchwork_page.filter_by_delegate('Nobody')
-  filtered_results = FilteredPatchResults(browser)
-  assert filtered_results.active_filters_contains('Delegate') > 1
+
+    patchwork_page = PatchworkHomePage(browser)
+    patchwork_page.load()
+    patchwork_page.show_filters()
+
+    patchwork_page.filter_by_delegate("Nobody")
+    filtered_results = FilteredPatchResults(browser)
+    assert filtered_results.active_filters_contains("Delegate") > 1
